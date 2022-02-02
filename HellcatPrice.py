384c41c2aa40d8cdcaec5b0d1f5ec20075234b7c
Adam76239 <97418619+Adam76239@users.noreply.github.com>
2022-02-02 08:08:03 -0600

    Add files via upload

---
+++ HellcatPrice.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.caranddriver.com/dodge/charger-srt-hellcat"
+
+get_url = requests.get(url)
+soup = BeautifulSoup(get_url.text,"html.parser")
+
+def extract_price(soup_obj, tag, attribute_name, attribute_value):
+    price = soup_obj.find(tag, {attribute_name: attribute_value}).text
+    return price
+
+price_point = extract_price(soup, "div", "class","msrp-value")
+
+print("The price of a 2022 Dodge Charger SRT Hellcat is:", price_point)
+
+# <div class="msrp-value">$78,595</div>
